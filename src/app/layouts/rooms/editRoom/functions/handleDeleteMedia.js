27822153e06391f6fb872b2120a4b248059a09ce
E-E-Ewoma-Zino <69873736+E-E-Ewoma-Zino@users.noreply.github.com>
2023-04-01 13:25:03 +0100

    initial commit

--- src/app/layouts/rooms/editRoom/functions/handleDeleteMedia.js
+++ src/app/layouts/rooms/editRoom/functions/handleDeleteMedia.js
@@ -1,31 +1,37 @@
 import { deleteRoomMedia } from "../../../../../api/delete";
 
-async function handleDeleteMedia(roomId, mediaPath, mediaType, setUpdateCount) {
-	console.log("delete", roomId, mediaPath, mediaType);
+async function handleDeleteMedia(
+  roomId,
+  cloud,
+  mediaPath,
+  mediaType,
+  setUpdateCount
+) {
+  console.log("delete", roomId, mediaPath, mediaType);
 
-	const data = { roomId, mediaPath, mediaType };
+  const data = { roomId, cloud, mediaPath, mediaType };
 
-	try {
+  try {
+    const deletedMedia = await (await deleteRoomMedia(data)).data;
 
-		const deletedMedia = await (await deleteRoomMedia(data)).data;
+    if (deletedMedia.data)
+      window.swalWithBootstrapButtons.fire({
+        icon: "success",
+        title: "Deleted " + mediaType,
+        text: "The" + mediaType + " has been deleted",
+        timer: 1000
+      });
+  } catch (err) {
+    window.swalWithBootstrapButtons.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+      footer: "<a href>Why do I have this issue?</a>"
+    });
+    console.error("Error from create room:", err.response.data);
+  }
 
-		if (deletedMedia.data) window.swalWithBootstrapButtons.fire({
-			icon: 'success',
-			title: 'Deleted ' + mediaType,
-			text: "The" + mediaType + " has been deleted",
-			timer: 1000
-		});
-	} catch (err) {
-		window.swalWithBootstrapButtons.fire({
-			icon: 'error',
-			title: 'Oops...',
-			text: 'Something went wrong!',
-			footer: '<a href>Why do I have this issue?</a>'
-		});
-		console.error("Error from create room:", err.response.data);
-	}
-
-	setUpdateCount(val => val + 1);
+  setUpdateCount((val) => val + 1);
 }
 
 export default handleDeleteMedia;
